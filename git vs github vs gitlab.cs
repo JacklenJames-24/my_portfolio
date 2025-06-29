520610f4b20c4723cfe006f9c3fb51181b008a04
Jacklen James <your_email@example.com>
2025-06-30 02:00:39 +0530

    first commit

--- git vs github vs gitlab.cs
+++
@@ -1,9 +0,0 @@
- git vs github vs gitlab
- git commands
- push code portfolio
- Solid principles
- Oops concepts (class object static abstractv interface polymorphissm, abstraction encapsulation, inheritance)
- Php basics
-
- namaste javascript
- 
